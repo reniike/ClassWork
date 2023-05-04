e1884289f3633d1f25b74226031d82d93deaeb0f
Babs-Olokode Aliyah <aliyaheniola91@gmail.com>
2023-05-04 16:24:17 +0100

    initial commit

--- src/chapterSeventeen/revision/StreamSampleOne.java
+++ src/chapterSeventeen/revision/StreamSampleOne.java
@@ -1,7 +1,10 @@
 package chapterSeventeen.revision;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class StreamSampleOne {
     public static List<Integer> mapMethod(List<Integer> numbers){
@@ -18,5 +21,35 @@ public class StreamSampleOne {
                 .collect(Collectors.toList());
     }
 
+    public static void randomNumbers() {
+        Random random = new Random();
+        System.out.println(Arrays.toString(IntStream
+                .generate(() -> 1 + random.nextInt(20))
+                .limit(15)
+                .toArray()));
+    }
 
+    public static void randomNumbersTwo(){
+        Random random = new Random();
+        System.out.println(IntStream
+                .generate(() -> 1 + random.nextInt(10))
+                .limit(5)
+                .boxed()
+                .toList());
+    }
+
+    public static List<String> randomNames(){
+        List<String> names = List.of("Renike", "Folahan", "Zainab", "Omotayo");
+       return names.stream()
+//                .map(name -> name.toUpperCase())
+                .map(String::toLowerCase)
+                .filter(x -> x.length() <= 6)
+                .toList();
+    }
+
+    public static void main(String[] args) {
+        randomNumbers();
+        randomNumbersTwo();
+        System.out.println(randomNames());
+    }
 }
